2e05e4d10e47ccf575dd0c6650b1b6f74cb38d88
Hugocod <u.medioli@icloud.com>
2022-11-09 09:08:44 +0100

    updated the show function

---
+++ resources/views/comics/show.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+
+    <h1>{{ $comic->title }}</h1>
+    <div>
+        <img src="{{ $comic->thumb }}" alt="{{ $comic->title }} cover">
+    </div>
+    <p>{{ $comic->description }}</p>
+
+</body>
+</html>
