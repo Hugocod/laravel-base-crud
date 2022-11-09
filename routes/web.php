507385ba905e4eddb8fcfea4257a6e64ba3409a6
Hugocod <u.medioli@icloud.com>
2022-11-09 20:50:38 +0100

    changed root view

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Comic;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,7 +15,8 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    $comics = Comic::all();
+    return view('comics.index', compact('comics'));
 });
 
 Route::resource('comics', 'ComicController');
